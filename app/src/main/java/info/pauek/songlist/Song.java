146f6ec4cde6b0552e7fe64c7862454b733da7cf
Sandra Alvarez <sandruskiag@gmail.com>
2018-10-25 10:44:38 +0200

    TODO 4. Modifico la classe Song per afegir els camps restants. Actualitzo el ViewHolder perquè mostri aquesta informació.

--- app/src/main/java/info/pauek/songlist/Song.java
+++ app/src/main/java/info/pauek/songlist/Song.java
@@ -3,8 +3,13 @@ package info.pauek.songlist;
 class Song {
     private String title;
 
-    public Song(String title) {
+    private String band;
+    private String year;
+
+    public Song(String title, String band, String year) {
         this.title = title;
+        this.band = band;
+        this.year = year;
     }
 
     public String getTitle() {
@@ -13,5 +18,21 @@ class Song {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
     }
 }
